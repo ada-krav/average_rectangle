2d4a2a4e0757eabfef1256408acccf3ac84e0ee5
Ada Kravchenko <ada.t.krav@gmail.com>
2025-04-18 00:52:48 +0300

    [NEW] allow to pass io strategy to ImageProcessor

--- tools/image_processor.py
+++ tools/image_processor.py
@@ -2,30 +2,29 @@ import cv2
 import numpy as np
 
 from abc import ABC, abstractmethod
+from av import VideoFrame
 from typing import Tuple, Optional, Dict
 
 
 class DrawingStrategy(ABC):
-
     def __init__(self, sizing_options: Optional[Dict] = None, *args, **kwargs):
         self.sizing_options = sizing_options or {}
 
     @abstractmethod
-    def draw(self, frame: np.ndarray, color: Tuple[int, int, int]) -> np.ndarray:
+    def draw(self, frame: np.ndarray, color_BGR: Tuple[int, int, int]) -> np.ndarray:
         pass
 
 
 class RectangleStrategy(DrawingStrategy):
-
     def __init__(self, sizing_options: Optional[Dict] = None, *args, **kwargs):
         super().__init__(sizing_options, *args, **kwargs)
         self.height_proportion = self.sizing_options.get("height_proportion", 0.3) #  30% by default
         self.width_proportion = self.sizing_options.get("width_proportion", 0.3)
 
-    def draw(self, frame: np.ndarray, color: Tuple[int, int, int]) -> np.ndarray:
+    def draw(self, frame: np.ndarray, color_BGR: Tuple[int, int, int]) -> np.ndarray:
         h, w, _ = frame.shape
         top_left, bottom_right = self._get_rectangle_coordinates(h, w)
-        cv2.rectangle(frame, top_left, bottom_right, color, cv2.FILLED)
+        cv2.rectangle(frame, top_left, bottom_right, color_BGR, cv2.FILLED)
         return frame
 
     def _get_rectangle_coordinates(
@@ -40,24 +39,57 @@ class RectangleStrategy(DrawingStrategy):
         return top_left, bottom_right
 
 
-class ImageProcessor:
+class IoStrategy(ABC):
+    @abstractmethod
+    def to_ndarray(self, image_data) -> np.ndarray:
+        pass
 
-    def __init__(self, strategy: Optional[DrawingStrategy] = None):
-        self._strategy = strategy or RectangleStrategy()
+    @abstractmethod
+    def from_ndarray(self, image_data: np.ndarray):
+        pass
 
-    def process_image(self, image_data: bytes, color: Tuple[int, int, int]) -> bytes:
-        if not (
-            isinstance(color, tuple)
-            and len(color) == 3
-            and all(0 <= c <= 255 for c in color)
-        ):
-            raise ValueError("Color must be a 3-tuple with values 0-255")
 
+class JpegIo(IoStrategy):
+    def to_ndarray(self, image_data) -> np.ndarray:
         nparr = np.frombuffer(image_data, np.uint8)
         frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
         if frame is None:
             raise ValueError("Failed to decode image")
+        return frame
 
-        frame = self._strategy.draw(frame, color[::-1])  # reversing to get BGR color tuple (OpenCV historically works with BGR)
+    def from_ndarray(self, frame: np.ndarray):
         _, buffer = cv2.imencode(".jpg", frame)
         return buffer.tobytes()
+
+
+class PyAVframeIo(IoStrategy):
+    def to_ndarray(self, image_data) -> np.ndarray:
+        return image_data.to_ndarray(format="bgr24")
+
+    def from_ndarray(self, frame: np.ndarray):
+        return VideoFrame.from_ndarray(frame, format="bgr24")
+
+
+class ImageProcessor:
+    def __init__(
+        self,
+        input_strategy: Optional[IoStrategy] = None,
+        output_strategy: Optional[IoStrategy] = None,
+        drawing_strategy: Optional[DrawingStrategy] = None,
+    ):
+        self._input_strategy = input_strategy or JpegIo()
+        self._output_strategy = output_strategy or input_strategy or JpegIo()
+        self._drawing_strategy = drawing_strategy or RectangleStrategy()
+
+    def process_image(self, image_data: bytes, color: Tuple[int, int, int]) -> bytes:
+        frame = self._input_strategy.to_ndarray(image_data)
+
+        if (
+            isinstance(color, tuple)
+            and len(color) == 3
+            and all(0 <= c <= 255 for c in color)
+        ):
+            frame = self._drawing_strategy.draw(frame, color_BGR=color[::-1])
+
+        output_frame = self._output_strategy.from_ndarray(frame)
+        return output_frame
