04f36f211ebd80621a0848e93b12ff700f20bea9
Ada Kravchenko <ada.t.krav@gmail.com>
2025-04-18 00:51:42 +0300

    [NEW] use react for frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WebRTCImageProcessing from './components/WebRTCImageProcessing';
+
+function App() {
+    return (
+        <div>
+            <WebRTCImageProcessing />
+        </div>
+    );
+}
+
+export default App;
